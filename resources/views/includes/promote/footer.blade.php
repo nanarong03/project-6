63a5843e5416428b5ff8fe836803a568c7108457
Nanarong throngsari <taerobot1029@gmail.com>
2022-09-13 14:23:03 +0700

    ปรับ CSS ใหม่หมด , คอมเม้นท์ footer หมด

--- resources/views/includes/promote/footer.blade.php
+++ resources/views/includes/promote/footer.blade.php
@@ -1,7 +1,7 @@
 <!-- footer -->
 <footer>
   <!-- newsletter -->
-  <div class="newsletter">
+  <!-- <div class="newsletter">
     <div class="container">
       <div class="row">
         <div class="col-md-9 ml-auto bg-primary py-5 newsletter-block">
@@ -16,29 +16,29 @@
         </div>
       </div>
     </div>
-  </div>
+  </div> -->
   <!-- footer content -->
-  <div class="footer bg-footer section border-bottom">
+  <!-- <div class="footer bg-footer section border-bottom">
     <div class="container">
       <div class="row">
-        <div class="col-lg-4 col-sm-8 mb-5 mb-lg-0">
+        <div class="col-lg-4 col-sm-8 mb-5 mb-lg-0"> -->
           <!-- logo -->
-          <a class="logo-footer" href="index.html"><img class="img-fluid mb-4" src="images/belt.png" alt="logo"
+          <!-- <a class="logo-footer" href="index.html"><img class="img-fluid mb-4" src="images/belt.png" alt="logo"
               width=250px></a>
           <ul class="list-unstyled">
             <li class="mb-4">VCG5+MC5 Unnamed Road ตำบล เสาธงหิน อำเภอบางใหญ่ นนทบุรี 11140</li>
             <li class="mb-2">090-945-7900</li>
             <li class="mb-2">tae0972126801@gmail.com</li>
           </ul>
-        </div>
+        </div> -->
         <!-- support -->
-        <div class="col-lg-2 col-md-3 col-sm-4 col-6 mb-5 mb-md-0">
+        <!-- <div class="col-lg-2 col-md-3 col-sm-4 col-6 mb-5 mb-md-0">
         </div>
       </div>
     </div>
-  </div>
+  </div> -->
   <!-- copyright -->
-  <div class="copyright py-4 bg-footer">
+  <!-- <div class="copyright py-4 bg-footer">
     <div class="container">
       <div class="row">
         <div class="col-sm-5 text-sm-right text-center">
@@ -49,6 +49,6 @@
         </div>
       </div>
     </div>
-  </div>
+  </div> -->
 </footer>
 <!-- /footer -->
